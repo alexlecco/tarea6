887824232b3df98aea61e4c2138bbdd93c636f38
Alex Lecco <alexlecco24@gmail.com>
2014-12-04 08:29:48 -0300

    First pages commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$("#btnclosemsg").click(function() {
+  $("#message").hide();
+});
+
+$("#btnclosepub").click(function() {
+  $("#publicity").hide();
+});
+
+$("#btnclosevideopost").click(function() {
+  $("#videopost").hide();
+});
